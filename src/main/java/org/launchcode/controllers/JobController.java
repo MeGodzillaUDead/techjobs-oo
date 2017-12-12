26e92ad95f4b375850072028504d0ca6be1b9bb0
Jonathan Riezman <riezman@gmail.com>
2017-12-12 14:35:38 -0600

    some more progress...need to fix redirect and Position Type

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,16 +1,21 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Employer;
 import org.launchcode.models.Job;
+import org.launchcode.models.data.JobFieldData;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
+
+import static org.launchcode.models.JobFieldType.EMPLOYER;
 
 /**
  * Created by LaunchCode
@@ -40,12 +45,24 @@ public class JobController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @Valid JobForm jobForm, Errors errors) {
+    public String add(Model model, @ModelAttribute @Valid JobForm jobForm, Errors errors) {
 
         // TODO #6 - Validate the JobForm model, and if valid, create a
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
+        if (!errors.hasErrors()) {
+            Job createdJob = new Job();
+            createdJob.setName(jobForm.getName());
+            jobData.setEmployerById(createdJob, jobForm.getEmployerId());
+            jobData.setLocationById(createdJob, jobForm.getLocationId());
+            jobData.setCoreCompetencyById(createdJob, jobForm.getCoreCompetencyId());
+            jobData.setPositionTypeById(createdJob, jobForm.getCoreCompetencyId());
 
+            jobData.add(createdJob);
+            return "redirect:?job=" + createdJob.getId();
+        }
+
+        model.addAttribute(new JobForm());
         return "";
 
     }
